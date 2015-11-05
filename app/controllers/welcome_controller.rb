ba6fd926c3e540a85c9b971765e013cbf41e48a1
lei7dover <limhoff@wastebits.com>
2015-11-04 20:26:31 -0500

    adding forms, validations, errors, etc

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,61 @@
 class WelcomeController < ApplicationController
+  before_action :set_posts
   def index
     @post = Post.all.sort_by{|p|p.created_at}.first
-    @posts = Post.all.reject{|p| p.id == @post.id}
     @page_title = @post.title
   end
 
   def show
     @post = Post.find(params[:id])
-    @posts = Post.where("id != ?",params[:id]).order("created_at DESC").limit(4)
     @page_title = @post.title
     render :template=> "welcome/index.html.erb"
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to welcome_index_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    render :new
+  end
+
+  def update
+    @post = Post.find(params[:id])
+      if @post.update(post_params)
+        redirect_to welcome_path @post
+      else
+        render :new
+      end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to welcome_index_path
+  end
+
+  private
+  def set_posts
+    if params[:id]
+      @posts = Post.where("id != ?",params[:id]).order("created_at DESC").limit(4)
+    else
+      @posts = Post.order("created_at DESC").limit(4)
+    end
+
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image, :author_id)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :author
+  validates_presence_of :title,:body, :author, :message => "missing"
 end
